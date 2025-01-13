1362856613ac5414d6497a678603132f66ccac51
Toni Maat <tmaat88@gmail.com>
2025-01-13 20:20:12 +0100

    login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    console.log('hahah');
+    if (this.loginForm.valid) {
+      const loginData = this.loginForm.value;
+      console.log('Login successful:', loginData);
+      // Add logic to handle authentication (e.g., API call)
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
--- src/app/navigation/navigation-item/navigation-item.component.ts
+++ src/app/navigation/navigation-item/navigation-item.component.ts
@@ -1,11 +1,11 @@
 import { Component, input, output } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-navigation-item',
-  imports: [RouterLink, FontAwesomeModule],
+  imports: [RouterLink, RouterLinkActive, FontAwesomeModule],
   templateUrl: './navigation-item.component.html',
   styleUrl: './navigation-item.component.scss'
 })
