fc6523ebc63b73216d19b95b382d097b9c7d6c1f
Toni Maat <tmaat88@gmail.com>
2025-01-16 12:53:17 +0100

    print username in dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,34 +1,39 @@
 import { Component, inject } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { createSelector, Store, createFeatureSelector } from '@ngrx/store';
 import { Observable } from 'rxjs';
 
 import { NavigationComponent } from '../navigation/navigation.component';
 import { UserInterface } from '../store/auth.reducer';
 import { Router } from '@angular/router';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
+
+export const selectAuthState = createFeatureSelector<UserInterface|null>('auth');
 
 @Component({
   selector: 'app-dashboard',
-  imports: [NavigationComponent, AsyncPipe],
+  imports: [NavigationComponent, AsyncPipe, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
   auth$: Observable<UserInterface | null>;
+  username$: Observable<string | null>;
   private router = inject(Router);
-
+  
   constructor(private store: Store<{auth: UserInterface|null}>) {
     this.auth$ = store.select('auth');
+    this.username$ = this.store.select(createSelector(selectAuthState, state => state?.username ?? ''));
     // console.log(this.auth$.value);
-    this.auth$.subscribe(() => {
-      (value: UserInterface|null) => {
-        console.log(';hahaha');
-        console.log(value);
-        if (value === null) {
-          this.router.navigate(['/']);
-        }
-      }
-    });
+    // this.auth$.subscribe(() => {
+    //   (value: UserInterface|null) => {
+    //     this.user = value;
+    //     console.log(';hahaha');
+    //     console.log(value);
+    //     if (value === null) {
+    //       this.router.navigate(['/']);
+    //     }
+    //   }
+    // });
   }
 
   ngOnInit() {
